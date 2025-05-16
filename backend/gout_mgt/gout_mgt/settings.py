75f4ce0aef74bf28c9cd5b4e3b7e91837974220f
kittjx <kittjx@hotmail.com>
2025-05-16 16:44:09 +0800

    switch to pgsql

--- backend/gout_mgt/gout_mgt/settings.py
+++ backend/gout_mgt/gout_mgt/settings.py
@@ -13,10 +13,6 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 
-import pymysql
-
-pymysql.install_as_MySQLdb()
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -91,12 +87,12 @@ WSGI_APPLICATION = 'gout_mgt.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('DJANGO_DB_NAME', 'devdb'),
-        'USER': os.getenv('DJANGO_DB_USER', 'devuser'),
-        'PASSWORD': os.getenv('DJANGO_DB_PASSWORD', 'devpass'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DJANGO_DB_NAME', 'proddb'),
+        'USER': os.getenv('DJANGO_DB_USER', 'produser'),
+        'PASSWORD': os.getenv('DJANGO_DB_PASSWORD', 'prodpass'),
         'HOST': os.getenv('DJANGO_DB_HOST', '127.0.0.1'),
-        'PORT': '3306',
+        'PORT': os.getenv('DJANGO_DB_PORT', '5432'),
     }
 }
 
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
 # Check if migrations exist
